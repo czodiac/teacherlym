9501f06a414f0a0bc45e91c4b46c631d4c0e71c3
czodiac <iltae80@daum.net>
2022-09-07 14:09:36 -0600

    defaultsite -> profile

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withPlugins([[withImages]], {
   async redirects() {
     return [
       {
-        source: '/components',
+        source: '/defaultsite',
         destination: '/profile',
         permanent: true,
       },
