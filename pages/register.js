ee04bcda68eec027d3611940577734e2150843c7
Iltae Lee <leeminsup@gmail.com>
2024-07-08 11:14:08 -0600

    Scroll to top after form is submitted

--- pages/register.js
+++ pages/register.js
@@ -1,12 +1,8 @@
-import React from "react";
-// nodejs divbrary that concatenates classes
-import classNames from "classnames";
-// @material-ui/core components
+import React, { useEffect } from "react";
 import { makeStyles } from "@material-ui/core/styles";
 import Header from "/components/Header/Header.js";
 import Footer from "/components/Footer/Footer.js";
 import HeaderLinks from "/components/Header/HeaderLinks.js";
-import Parallax from "/components/Parallax/Parallax.js";
 import styles from "/styles/jss/nextjs-material-kit/pages/profilePage.js";
 
 const useStyles = makeStyles(styles);
@@ -14,6 +10,25 @@ const useStyles = makeStyles(styles);
 export default function HomePage(props) {
   const classes = useStyles();
   const { ...rest } = props;
+
+  useEffect(() => {
+    const iframe = document.querySelector("iframe");
+
+    const onIframeLoad = () => {
+      window.scrollTo(0, 0);
+    };
+
+    if (iframe) {
+      iframe.addEventListener("load", onIframeLoad);
+    }
+
+    return () => {
+      if (iframe) {
+        iframe.removeEventListener("load", onIframeLoad);
+      }
+    };
+  }, []);
+
   return (
     <div>
       <Header rightLinks={<HeaderLinks />} fixed {...rest} />
@@ -22,13 +37,13 @@ export default function HomePage(props) {
         src="https://docs.google.com/forms/d/e/1FAIpQLSf7mW6uvXGJwdAyY65sTLst5WScwqq07Bp93zHxCXYtqKlW0w/viewform?embedded=true"
         width="100%"
         height="3868"
-        frameborder="0"
-        marginheight="0"
-        marginwidth="0"
+        frameBorder="0"
+        marginHeight="0"
+        marginWidth="0"
+        onLoad={() => window.scrollTo(0, 0)} // Ensures scroll to top on iframe load
       >
         Loading…
       </iframe>
-
       <Footer />
     </div>
   );
